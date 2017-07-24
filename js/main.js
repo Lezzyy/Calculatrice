80d9f499a4bae83d8e5c22bc5676c767a90cf1cc
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-24 15:21:20 +0200

    number ok

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 //VARIABLES
 var number = document.getElementsByClassName('button');
-var sign = document.getElementsByClassName('buttonFirst');
+var multi = document.getElementsByClassName('multi');
+var add = document.getElementsByClassName('add');
+var minus = document.getElementsByClassName('minus');
+var dot = document.getElementsByClassName('dot');
+var suppr = document.getElementsByClassName('suppr');
 var result = document.getElementById('result');
 var calcul = [];
 
 
 
-function val(touches) {
-  for (let i = 0; i < touches.length; i++) {
-    touches[i].onclick = function() {
-      calcul.push(touches[i].innerHTML);
-      result.innerHTML = calcul;
+
+function val(number) {
+
+  for (let i = 0; i < number.length; i++) {
+    number[i].onclick = function() {
+      var numbers = parseFloat(number[i].innerHTML);
+      calcul.push(numbers);
+      result.innerHTML = calcul.join("");
+      console.log(calcul);
     }
   }
 }
+
+
 val(number);
-val(sign);
